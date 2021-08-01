374c1878e4688e516044bfea7bd0e3fe2d53c615
Jet <Jet@163.com>
2021-08-01 18:06:18 +0800

    master1 commit

--- src/main/java/gitTest.java
+++ src/main/java/gitTest.java
@@ -8,5 +8,6 @@ public class gitTest {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("abc");
+        System.out.println("push");
     }
 }
